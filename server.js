014a667e6bd00d99a5387f2244b3918f52cc6fe1
yashraj.exe <yashraj.s2711@gmail.com>
2023-05-16 13:54:16 +0530

    fix cors error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const connectDB = require('./config/connection');
 app.use(express.json())
-app.use(cors());
+app.use(cors({origin : "*"}));
 app.use(express.urlencoded({extended:true}))
 connectDB();
 
